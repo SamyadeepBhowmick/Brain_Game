81cec782544f73dd3587eda2284c564276420ac0
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2019-10-02 17:38:58 +0530

    Apply Validation in javascript

--- js/welcome.js
+++ js/welcome.js
@@ -2,12 +2,18 @@ var n;
 var person=new Array();
 var person1=new Array();
 function limit(){
-    n=prompt("Enter the limit");
-    mFunction(n)
+    var str1;
+    str1=prompt("Enter the limit");
+    while(str1.length == 0 || !(str1.charCodeAt(0)>47 && str1.charCodeAt(0)<58)){
+        str1= prompt("Enter any number");     
+    }
+    n=parseInt(str1);
+    document.getElementById("demo1").innerHTML = null;
+    mFunction();
 }
 
-function mFunction(v) { 
-    for (i=0; i<v; i++)
+function mFunction() { 
+    for (i=0; i<n; i++)
     {
         person[i] = Math.floor(Math.random() * 10);
     }
@@ -32,17 +38,21 @@ function mFunction(v) {
         }
     }, 5000);
 
-    //document.getElementById("abcd").style.visibility = "visible";
     document.getElementById("abc").disabled=true;
 
 
 }
 function myFunction(){
     document.getElementById("demo").innerHTML = "";
+    var str;
     for (i=0; i<n; i++)
     {
                         
-        person1[i] = prompt("Enter the sequence number:"+(i+1));
+        str = prompt("Enter the sequence number:"+(i+1));
+        while(str.length != 1 || !(str.charCodeAt(0)>47 && str.charCodeAt(0)<58)){
+            str= prompt("Wrong Input!\nEnter only one numeric character.\nFor Example:3");     
+        }
+        person1[i]=parseInt(str);
     }
     var nw;
     nw=0;
@@ -61,6 +71,8 @@ function myFunction(){
 
     document.getElementById("demo").innerHTML ="Random Sequence was:    "+person;
     document.getElementById("demo1").innerHTML ="Your Input Sequence is:  "+person1;
-    document.getElementById("abcd").disabled=true;
-    
+    document.getElementById("abc").disabled=false;
+    document.getElementById("abcd").style.visibility="hidden";
+    person=new Array();
+    person1=new Array();
 }
